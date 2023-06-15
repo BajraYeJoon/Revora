dac0e1773ff7d4b90671b5d38b96e46efacc0b4b
bajrayejoon <binaya.bajrashakya@gmail.com>
2023-06-15 15:48:49 +0545

    feat(navbar ui added): navbar, static links

---
+++ app/sections/Navbar/Navbar.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import Wrapper from "../Wrapper";
+import Logo from "./Logo";
+import Search from "./Search";
+import Menu from "./Menu";
+
+interface NavbarProps {}
+
+const Navbar: React.FC<NavbarProps> = () => {
+  return (
+    <div className="fixed z-10 w-full bg-white shadow-sm">
+      <div
+        className="
+       border-b-[1px]
+       py-4
+      "
+      >
+        <Wrapper>
+          <div className="flex flex-row items-center justify-between gap-4 md:gap-0">
+            <Search />
+            <Logo />
+            <Menu />
+          </div>
+        </Wrapper>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ app/sections/Wrapper.tsx
@@ -0,0 +1,25 @@
+"use client";
+// -----------------------------------------------------------------------------for client side to styling
+
+interface WrapperProps {
+  children: React.ReactNode;
+}
+
+const Wrapper: React.FC<WrapperProps> = ({ children }) => {
+  return (
+    <div
+      className="
+     mx-auto
+     max-w-[1280px]
+     px-4
+     sm:px-2
+     md:px-10
+     xl:px-20
+   "
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Wrapper;
