2e9f6c30b88797148ca070a899de461a14c95796
Binaya Bajracharya <binaya.bajrashakya@gmail.com>
2023-12-11 09:56:55 +0000

    Refactor layout and navbar components, and simplify wrapper styling

--- app/sections/Navbar/Navbar.tsx
+++ app/sections/Navbar/Navbar.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import Wrapper from "../Wrapper";
 import Logo from "./Logo";
 import Search from "./Search";
@@ -7,25 +5,18 @@ import Menu from "./Menu";
 
 interface NavbarProps {}
 
-const Navbar: React.FC<NavbarProps> = () => {
-  return (
-    <div className="fixed z-10 w-full bg-white shadow-sm">
-      <div
-        className="
-       border-b-[1px]
-       py-4
-      "
-      >
-        <Wrapper>
-          <div className="flex flex-row items-center justify-between gap-4 md:gap-0">
-            <Search />
-            <Logo />
-            <Menu />
-          </div>
-        </Wrapper>
-      </div>
+const Navbar: React.FC<NavbarProps> = () => (
+  <div className="fixed z-10 w-full bg-white shadow-sm">
+    <div className="border-b-[1px] py-4">
+      <Wrapper>
+        <div className="flex flex-row items-center justify-between gap-4 md:gap-0">
+          <Search />
+          <Logo />
+          <Menu />
+        </div>
+      </Wrapper>
     </div>
-  );
-};
+  </div>
+);
 
 export default Navbar;
--- app/sections/Wrapper.tsx
+++ app/sections/Wrapper.tsx
@@ -7,16 +7,7 @@ interface WrapperProps {
 
 const Wrapper: React.FC<WrapperProps> = ({ children }) => {
   return (
-    <div
-      className="
-     mx-auto
-     max-w-[1280px]
-     px-4
-     sm:px-2
-     md:px-10
-     xl:px-20
-   "
-    >
+    <div className="mx-auto max-w-[1280px] px-4 sm:px-2 md:px-10 xl:px-20">
       {children}
     </div>
   );
